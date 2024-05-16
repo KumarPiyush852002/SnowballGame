7d1499c39fbedf25c677e6af8aa77bb490fe861c
KumarPiyush852002 <75292115+KumarPiyush852002@users.noreply.github.com>
2024-05-17 00:30:09 +0530

    map loader

--- src/index.js
+++ src/index.js
@@ -6,12 +6,23 @@ const app=express();
 const httpServer = createServer(app);
 
 const io = new Server(httpServer);
+const loadMap=require("./mapLoader");
+
+
+// if you have a file
+async function main() {
+    
+    const map2D=await loadMap();
 
 io.on("connect", (socket) =>{
     console.log("user connected",socket.id);
-})
+    
+    socket.emit("map",map2D);
+});
 
 app.use(express.static("public"));
 
 
 httpServer.listen(5000);
+}
+main();
