d495583047352b75640b88af6f783b9e63fe725c
KumarPiyush852002 <75292115+KumarPiyush852002@users.noreply.github.com>
2024-05-31 22:25:42 +0530

    adding movements

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 const mapImage=new Image();
 mapImage.src="/snowy-sheet.png";
 
+const santaImage=new Image();
+santaImage.src="/santa.png";
+
 const canvasEl=document.getElementById('canvas');
 canvasEl.width=window.innerWidth;
 canvasEl.height=window.innerHeight;
@@ -9,6 +12,7 @@ const canvas=canvasEl.getContext("2d");
 const socket=io(`ws://localhost:5000`);
 
 let map=[[]];
+let players=[];
 const TILE_SIZE=32;
 
 socket.on('connect',()=>{
@@ -17,6 +21,43 @@ socket.on('connect',()=>{
 
 socket.on('map',(loadedMap)=>{
     map=loadedMap;
+});
+
+socket.on('players',(serverPlayers)=>{
+    players=serverPlayers;
+})
+
+const inputs={
+    up: false,
+    down: false,
+    left: false,
+    right: false
+};
+
+window.addEventListener("keydown",(e)=>{
+    if (e.key==="w") {
+        inputs["up"]=true;
+    } else if (e.key ==="s"){
+        inputs["down"]=true;
+    } else if (e.key ==="a"){
+        inputs["left"]=true;
+    } else if (e.key==="d"){
+        inputs["right"]=true;
+    }
+    socket.emit('inputs',inputs);
+});
+
+window.addEventListener("keyup",(e)=>{
+    if (e.key==="w") {
+        inputs["up"]=false;
+    } else if (e.key ==="s"){
+        inputs["down"]=false;
+    } else if (e.key ==="a"){
+        inputs["left"]=false;
+    } else if (e.key==="d"){
+        inputs["right"]=false;
+    }
+    socket.emit('inputs',inputs);
 });
 
 function loop() {
@@ -41,6 +82,9 @@ function loop() {
                 TILE_SIZE
             )
         }
+    }
+    for(const player of players){
+    canvas.drawImage(santaImage,player.x,player.y);
     }
     window.requestAnimationFrame(loop);
 }
